ee9433c6394c9333d5d2d6e3c8ce904cbdae9d99
Devanshgarg <dgarg3@academic.rrc.ca>
2026-02-23 15:30:52 -0600

    Added employee repository to manage department and employee data

---
+++ lab_1.2/src/repositories/employeeRepo.ts
@@ -0,0 +1,21 @@
+import { departments as initialDepartments } from "../data/employees";
+import type { Department } from "../interfaces/Department";
+import type { Employee } from "../interfaces/Employee";
+
+let departments: Department[] = [...initialDepartments];
+
+export const employeeRepo = {
+  getDepartments(): Department[] {
+    return departments;
+  },
+
+  createEmployee(employee: Employee, departmentName: string): Department[] {
+    departments = departments.map((dep) =>
+      dep.name === departmentName
+        ? { ...dep, employees: [...dep.employees, employee] }
+        : dep
+    );
+
+    return departments;
+  },
+};
