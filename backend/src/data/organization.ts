30b9de5ae3872aee8f65f6087a1544674339b45e
Devanshgarg <dgarg3@academic.rrc.ca>
2026-03-13 12:58:59 -0500

    added organization roles data

---
+++ backend/src/data/organization.ts
@@ -0,0 +1,7 @@
+export const roles = [
+  { firstName: "Jo-Anne", lastName: "Sinclair", role: "CEO/Chair of Board" },
+  { firstName: "Jackson", lastName: "Smith", role: "COO/VP Operations" },
+  { firstName: "Susan", lastName: "Thomas", role: "CFO/VP Administration" },
+  { firstName: "Richa", lastName: "Kaur", role: "VP Client Services" },
+  { firstName: "Josee", lastName: "Benjamin", role: "CIO" }
+];
