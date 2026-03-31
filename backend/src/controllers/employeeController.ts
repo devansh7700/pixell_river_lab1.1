523b4d11f4aaa63fd744409d5adc1ebe3659f403
Devanshgarg <dgarg3@academic.rrc.ca>
2026-03-30 21:16:35 -0500

    Replaced employee repository local data with prisma database queries

---
+++ backend/src/controllers/employeeController.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function getEmployees() {
+  return prisma.department.findMany({
+    include: {
+      employees: true,
+    },
+  });
+}
+
+export async function createEmployee(
+  firstName: string,
+  department: string
+) {
+  const dep = await prisma.department.findUnique({
+    where: {
+      name: department,
+    },
+  });
+
+  if (!dep) {
+    return { error: "Department not found" };
+  }
+
+  await prisma.employee.create({
+    data: {
+      firstName,
+      departmentId: dep.id,
+    },
+  });
+
+  return { success: true };
+}
