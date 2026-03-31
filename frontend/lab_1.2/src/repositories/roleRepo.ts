42b41ba9c3144e62bc510be7d0f95ca8eacaf397
Devanshgarg <dgarg3@academic.rrc.ca>
2026-03-30 21:39:28 -0500

    updated frontend role repository for backend database integration

--- frontend/lab_1.2/src/repositories/roleRepo.ts
+++ frontend/lab_1.2/src/repositories/roleRepo.ts
@@ -1,8 +1,11 @@
 export const roleRepo = {
-
   async getRoles() {
     const response = await fetch("http://localhost:3000/roles");
-    return response.json();
-  }
 
+    if (!response.ok) {
+      throw new Error("Failed to fetch roles");
+    }
+
+    return await response.json();
+  },
 };
