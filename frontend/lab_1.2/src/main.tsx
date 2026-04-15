f38978821eede6a108b86470b7c2b9847377ded2
Devanshgarg <dgarg3@academic.rrc.ca>
2026-04-15 08:55:35 -0500

    added publishable key in .env and configured clerk provider in main entry file

--- frontend/lab_1.2/src/main.tsx
+++ frontend/lab_1.2/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const clerkPubKey =
+  import.meta.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <App />
+    </ClerkProvider>
+  </StrictMode>
+);
