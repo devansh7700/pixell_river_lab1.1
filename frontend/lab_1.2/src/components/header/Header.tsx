684c55c00a915a8054d6105db6055bbe48009d1f
Devanshgarg <dgarg3@academic.rrc.ca>
2026-04-15 09:14:21 -0500

    added clerk sign in and user controls to header navigation

--- frontend/lab_1.2/src/components/header/Header.tsx
+++ frontend/lab_1.2/src/components/header/Header.tsx
@@ -1,10 +1,22 @@
 import "./Header.css";
+import {SignedIn,SignedOut,SignInButton,UserButton,} from "@clerk/clerk-react";
 
 function Header() {
   return (
     <header className="site-header">
       <h1>Pixell River Employee Directory</h1>
       <p>Welcome to the Pixell River Financial staff directory.</p>
+
+      <div style={{ marginTop: "1rem" }}>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+      
     </header>
   );
 }
